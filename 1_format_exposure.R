58b6429dbc4448ecc2d0fb6f992a93bea4ea887a
laxmibhatta <49127542+laxmibhatta@users.noreply.github.com>
2020-10-11 10:05:21 +0200

    Update 1_format_exposure.R

--- 1_format_exposure.R
+++ 1_format_exposure.R
@@ -1,1 +1,102 @@
+# 2-Sample MR study
+library(data.table)
+library(devtools)
+library(TwoSampleMR) # install_github("MRCIEU/TwoSampleMR")
+library(MRInstruments)
+library(ieugwasr)
+library(MendelianRandomization)
+library(MRPRESSO)
+library(ggplot2)
+
+options(stringsAsFactors=FALSE)
+library(data.table)
+library(optparse)
+library(GenABEL)
+
+option_list <- list(
+  make_option("--input", type="character", default="",
+    help="instrument downloaded from the source, [default='']"),
+  make_option("--output", type="character", default="./output",
+    help="Output directory of the files formatted for TwoSampleMR, [default='~/output']"),
+  make_option("--Phenotype", type="character", default="",
+    help="exposure variable, [default='']"),
+  make_option("--SNP", type="character", default="",
+    help="RSID/rs number of the SNP, [default='']"),
+  make_option("--beta", type="character", default="",
+    help="instrument downloaded from the source, [default='']"),
+  make_option("--se", type="character", default="",
+    help="standard error of the estimate, [default='']"),
+  make_option("--eaf", type="character", default="",
+    help="effect/alternate allele frequency, [default='']"),
+  make_option("--effect_allele", type="character", default="",
+    help="effect/alternate allele, [default='']"),
+  make_option("--other_allele", type="character", default="",
+    help="reference allele, [default='']"),
+  make_option("--pval", type="character", default="",
+    help="p value of the estimate, [default='']"),
+  make_option("--chr", type="character", default="",
+    help="chromosome number, [default='']"),
+  make_option("--position", type="character", default="",
+    help="base pair position of SNP, [default='']"),
+  make_option("--units", type="character", default="",
+    help="measurement unit, [default='']"),
+  make_option("--ncase", type="character", default="",
+    help="numbers of cases, [default='']"),
+  make_option("--ncontrol", type="character", default="",
+    help="numbers of controls, [default='']"),
+  make_option("--samplesize", type="character", default="",
+    help="sample size of the study, [default='']"),
+  make_option("--gene", type="character", default="",
+    help="name of the gene, [default='']"),
+  make_option("--id", type="character", default="",
+    help="id of the variant, [default='']"),
+  make_option("--outdir", type="character", default="./output",
+    help="Output directory [default='~/output']")
+)
+
+parser <- OptionParser(usage="%prog [options]", option_list=option_list)
+
+args <- parse_args(parser, positional_arguments = 0)
+opt <- args$options
+print(opt)
+
+output <- opt$output
+if(!file.exists(output)) system(paste("mkdir -p",output))
+
+outdir <- opt$outdir
+if(!file.exists(outdir)) system(paste("mkdir -p",outdir))
+
+# Format the file for TwoSampleMR
+file <- fread(opt$input, sep="\t",header=TRUE,stringsAsFactor=FALSE)
+
+if(!opt$Phenotype == "NA") file$Phenotype <- opt$Phenotype
+if(!opt$SNP == "NA") colnames(file)[colnames(file) == opt$SNP] <- "SNP"
+if(!opt$beta == "NA") colnames(file)[colnames(file) == opt$beta] <- "beta"
+if(!opt$se == "NA") colnames(file)[colnames(file) == opt$se] <- "se"
+if(!opt$eaf == "NA") colnames(file)[colnames(file) == opt$eaf] <- "eaf"
+if(!opt$effect_allele == "NA") colnames(file)[colnames(file) == opt$effect_allele] <- "effect_allele"
+if(!opt$other_allele == "NA") colnames(file)[colnames(file) == opt$other_allele] <- "other_allele"
+if(!opt$pval == "NA") colnames(file)[colnames(file) == opt$pval] <- "pval"
+if(!opt$chr == "NA") colnames(file)[colnames(file) == opt$chr] <- "chr"
+if(!opt$position == "NA") colnames(file)[colnames(file) == opt$position] <- "position"
+if(!opt$units == "NA") file$units <- opt$units
+if(!opt$ncase == "NA") colnames(file)[colnames(file) == opt$ncase] <- "ncase"
+if(!opt$ncontrol == "NA") colnames(file)[colnames(file) == opt$ncontrol] <- "ncontrol"
+if(!opt$samplesize == "NA") colnames(file)[colnames(file) == opt$samplesize] <- "samplesize"
+if(!opt$gene == "NA") colnames(file)[colnames(file) == opt$gene] <- "gene"
+if(!opt$id == "NA") colnames(file)[colnames(file) == opt$id] <- "id"
+
+write.table(file, paste0(output, "/", opt$Phenotype, ".txt"), sep = "\t", quote=FALSE, col.names = TRUE, row.names = FALSE)
+
+file <- as.data.frame(file, fix.empty.names=FALSE)
+
+file <- format_data(file, type="exposure")
+file <- subset(file, file$pval.exposure<0.00000005,)
+
+head(file)
+
+# Clumping
+file <- clump_data(file) # (remove SNPs -  R2 greater than 0.001)
+write.table(file, paste0(output,"/",opt$Phenotype, "_clumped.txt"), sep = "\t", quote=F, col.names = TRUE, row.names = FALSE)
+
 
